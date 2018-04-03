73a65ba0964a48eb4473eda028835b3ec6c6e13d
Roman Kryvun <kryvun.roman@gmail.com>
2018-04-03 19:23:50 +0300

    Fixed client/index.js

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -4,4 +4,6 @@ class App extends Component {
   render() {
     return <h1>Hello World</h1>;
   }
-}
+};
+
+export default App;
--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from "react-dom";
 import App from './components/App.jsx';
 
-import styles from './styles/main.css';
+import './styles/main.css';
 
-render(
+ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
