5d9d7662dbf8ea529355f81223ae7e91c64ec699
Roman Kryvun <kryvun.roman@gmail.com>
2018-04-03 19:45:37 +0300

    Added base styles.

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import App from './components/App.jsx';
 
-import './styles/main.css';
+import './assets/css/main.css';
 
 ReactDOM.render(
   <App />,
